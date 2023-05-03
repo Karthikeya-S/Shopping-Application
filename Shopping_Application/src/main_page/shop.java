971b2267305f8eecf9da838fe7293feaa6640c2a
Karthikeya S <84449739+Karthikeya-S@users.noreply.github.com>
2023-05-03 10:50:21 +0530

    Add files via upload

---
+++ Shopping_Application/src/main_page/shop.java
@@ -0,0 +1,15 @@
+package main_page;
+
+import java.util.*;
+import java.io.*;
+
+public class shop {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("WELCOME TO ONLINE SHOPPING SYSTEM\n");
+		System.out.println("*******************************************************");
+		System.out.println("\n1.REGISTER AS ADMIN\n2.REGISTER AS CUSTOMER\n3.LOGIN TO SYSTEM\n4.EXIT");
+		sc.close();
+	}
+}
